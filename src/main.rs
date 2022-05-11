55f45845bd345878c2e37e1474a58c2c9ef4bbf0
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2022-05-11 10:59:40 +0900

    feat: 1のときは1を返す

--- src/main.rs
+++ src/main.rs
@@ -2,16 +2,23 @@ fn main() {
     println!("Hello, world!");
 }
 
+fn target_method(arg: i32) -> String {
+    return arg.to_string();
+}
+
 // https://doc.rust-jp.rs/book-ja/ch11-01-writing-tests.html
 #[cfg(test)]
 mod tests {
+
+    use super::*;
+
     #[test]
     fn it_works() {
         assert_eq!(2 + 2, 4);
     }
-    
+
     #[test]
-    fn it_failed() {
-        assert_eq!(2 + 2, 1);
+    fn _1のときは1を取得する() {
+        assert_eq!(target_method(1), "1");
     }
 }
