171f9a6a40252fe7853e37ed663594a1fd3086eb
Tomoaki Takaichi <kiruhihhohu555@gmail.com>
2022-05-11 10:43:42 +0900

    feat: todo and confirm test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
 }
+
+// https://doc.rust-jp.rs/book-ja/ch11-01-writing-tests.html
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+    
+    #[test]
+    fn it_failed() {
+        assert_eq!(2 + 2, 1);
+    }
+}
